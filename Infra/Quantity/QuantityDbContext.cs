2589b1989627ad07749445c31f438a79d4da841e
ellomp <60709145+ellomp@users.noreply.github.com>
2020-02-26 21:10:39 +0200

    8. osa - Andmebaasi tabelite loomine tehtud

--- Infra/Quantity/QuantityDbContext.cs
+++ Infra/Quantity/QuantityDbContext.cs
@@ -5,7 +5,7 @@ namespace Abc.Infra.Quantity
 {
     public class QuantityDbContext : DbContext
     {
-        public DbSet<MeasureData> Measures { get; set; }
+        public DbSet<MeasureData> Measures { get; set; }//need on kõik data setid ja need kõik hoiavad endas andmeid.
         public DbSet<UnitData> Units { get; set; }
         public DbSet<SystemOfUnitsData> SystemsOfUnits { get; set; }
         public DbSet<UnitFactorData> UnitFactors { get; set; }
@@ -27,7 +27,7 @@ namespace Abc.Infra.Quantity
             builder.Entity<MeasureData>().ToTable(nameof(Measures)); 
             builder.Entity<UnitData>().ToTable(nameof(Units));
             builder.Entity<SystemOfUnitsData>().ToTable(nameof(SystemsOfUnits));
-            builder.Entity<UnitFactorData>().ToTable(nameof(UnitFactors));
+            builder.Entity<UnitFactorData>().ToTable(nameof(UnitFactors)).HasKey(x=>new{x.UnitId, x.SystemOfUnitsId});
 
         }
     }
