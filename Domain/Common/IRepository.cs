78ce0283433f327e669551c91eddc64fc11d52ea
ellomp <60709145+ellomp@users.noreply.github.com>
2020-02-29 08:32:47 +0200

    Interfaced loodud ja refaktooritud

--- Domain/Common/IRepository.cs
+++ Domain/Common/IRepository.cs
@@ -1,20 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace Abc.Domain.Common
+﻿namespace Abc.Domain.Common
 {
-    public interface IRepository<T>
+    public interface IRepository<T> : ICrudMethods<T>, IPaging, ISorting, ISearching
     {
-        Task<List<T>> Get();
-        Task<T> Get(string Id);
-        Task Delete(string Id);
-        Task Add(T obj);
-        Task Update(T obj);
-        string SortOrder { get; set; }
-        string SearchString { get; set; }
-        int PageIndex { get; set; }
-        bool HasNextPage { get; set; }
-        bool HasPreviousPage { get; set; }
-
+       
+       
     }
 }
---
+++ Domain/Common/ISorting.cs
@@ -0,0 +1,8 @@
+﻿namespace Abc.Domain.Common
+{
+    public interface ISorting
+    {
+        string SortOrder { get; set; }
+
+    }
+}
